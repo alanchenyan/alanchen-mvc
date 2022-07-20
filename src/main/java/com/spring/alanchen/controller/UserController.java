46e6d0e6957968371af957cc802a92eb1a79e2ee
chenyan <271961730@qq.com>
2022-07-20 08:11:29 +0800

    Update UserController.java

--- src/main/java/com/spring/alanchen/controller/UserController.java
+++ src/main/java/com/spring/alanchen/controller/UserController.java
@@ -13,7 +13,7 @@ import com.spring.alanchen.service.IUserSerivce;
 /**
  * @author Alan Chen
  * @description 访问：http://127.0.0.1:8080/alanchen-mvc/user/query?name=ac&age=18
- * @date 2020-07-28
+ * @date 2020-07-29
  */
 @AlanChenController
 @AlanChenRequestMapping("/user")
